834aac8f4b68a0f39ee90e6b9376174761e76be9
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-29 14:48:56 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": [2, { devDependencies: true }],
+    "react/forbid-prop-types": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0
+  },
+};
