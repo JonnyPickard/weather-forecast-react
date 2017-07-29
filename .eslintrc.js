8547faefae633c6ab348b78e5690f61aab15ca27
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-29 15:50:47 +0100

    Updated SearchBar to prevent default

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "import/no-extraneous-dependencies": [2, { devDependencies: true }],
     "react/forbid-prop-types": 0,
-    "jsx-a11y/no-noninteractive-element-interactions": 0
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "class-methods-use-this": 0,
+    "react/prefer-stateless-function": 0
   },
+  "env": {
+    "browser": true
+  }
 };
